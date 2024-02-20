c96479aa83c6f07e5581cab2171782809f899ccd
Carlos Saavedra (Koru) <carsaavedrapa@gmail.com>
2024-02-19 22:24:08 -0500

    refactor(main-page): change the content h1

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Welcome to the Next.js + Tailwind CSS Starter Kit</h1>
+      <h1>Hello world Project to get that job</h1>
     </main>
   );
 }
